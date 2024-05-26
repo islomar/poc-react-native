0b923777b20abadedd228aa79105e5fb8317bd22
Isidro Lopez <isidro.lopez@paired.com>
2024-05-26 20:38:14 +0200

    Objetos literales e interfaces

--- react-native-devtalles-course/01-react-foundation/src/App.tsx
+++ react-native-devtalles-course/01-react-foundation/src/App.tsx
@@ -1,10 +1,10 @@
-import { BasicTypes } from "./typescript"
+import {BasicTypes, ObjectLiterals} from "./typescript"
 
 function App() {
   return (
     <main>
         <h1>Introduction to React</h1>
-
+        <ObjectLiterals />
         <BasicTypes />
     </main>
   )
--- react-native-devtalles-course/01-react-foundation/src/typescript/ObjectLiterals.tsx
+++ react-native-devtalles-course/01-react-foundation/src/typescript/ObjectLiterals.tsx
@@ -1,5 +1,33 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+    address: Address;
+    isAlive?: boolean;
+}
+
+interface Address {
+  country: string;
+  houseNumber: number;
+}
+
 export const ObjectLiterals = () => {
+  const person: Person = {
+    age: 48,
+    firstName: "Isidro",
+    lastName: "Lopez",
+    address: {
+      country: "Spain",
+      houseNumber: 42
+    }
+  }
+
   return (
-    <div>ObjectLiterals</div>
+      <>
+        <h3>Objetos literales</h3>
+        <pre>
+          {JSON.stringify(person, null, 2)}
+        </pre>
+      </>
   )
 }
